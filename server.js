1f41df55482d6aefa9cf2e43adac443665eaa0fb
ndelvalle <nicolas.delvalle@gmail.com>
2015-10-29 00:20:36 -0300

    Init package.json and added server the main js file

---
+++ server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var config = require('./config/config');
+var port   = config.PORT;
+
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+var morgan     = require('morgan');
+var mongoose   = require('mongoose');
+
+var route      = require('./route');
+var middleware = require('./middleware');
+var logger     = require('./libraries/logger');
+
+var Promise = require('bluebird');
+Promise.promisifyAll(mongoose);
+
+if (config.DEV_MODE) {
+  mongoose.set('debug', true);
+}
+
+mongoose.connect(config.MONGODB_URL);
+
+app.set('superSecret', config.SECRET);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(middleware.cors);
+app.use('/', route);
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
+
+module.exports = app;
