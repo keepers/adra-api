2f12ca3b9444e87f99a067859f392b9abead040f
ndelvalle <nicolas.delvalle@gmail.com>
2016-01-20 01:16:50 +0100

    Fixed jshint warning adding missing semicolon

--- config/config.js
+++ config/config.js
@@ -6,4 +6,4 @@ module.exports = {
 	JWT_EXPIRATION: process.env.JWT_EXPIRATION ||  1440,
 	MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/adra',
 	ENV: process.env.NODE_ENV || 'development'
-}
+};
